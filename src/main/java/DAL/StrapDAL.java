81c94e3874ebcb62ec8699edfb78e28d11df4ed8
jukute2000 <81506091+jukute2000@users.noreply.github.com>
2022-11-06 21:47:11 +0700

    update

--- src/main/java/DAL/StrapDAL.java
+++ src/main/java/DAL/StrapDAL.java
@@ -6,7 +6,10 @@ package DAL;
 
 import Entity.Strap;
 import Utils.HibernateUtils;
+import java.io.Serializable;
+import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 /**
  *
@@ -14,11 +17,50 @@ import org.hibernate.Session;
  */
 public class StrapDAL {
 
-    private static Session session = HibernateUtils.getSessionFactory().openSession();
+    private static Session session;
+
+    public StrapDAL() {
+        session = HibernateUtils.getSessionFactory().openSession();
+    }
 
     public static void addStrap(Strap st) {
         session.beginTransaction();
         session.save(st);
         session.getTransaction().commit();
+    }
+
+    public void deleteStrap(int id) {
+        session.beginTransaction();
+        Strap str = new Strap();
+        str.setStrapID(id);
+        session.merge(str);
+        session.delete(str);
+        session.getTransaction().commit();
+    }
+    public void strapdele(int strapID)
+    {
+        session.beginTransaction();
+        Query q = session.createQuery("Delete * FROM strap WHERE StrapID = :strapID");
+        q.setParameter("strapID", strapID);
+        session.getTransaction().commit();
+    }
+    public void updateStrap(Strap obj) {
+        session.update(obj);
+    }
+
+    public Strap getStrap(int StrapID) {
+        Strap obj;
+        session.beginTransaction();
+        obj = session.get(Strap.class, StrapID);
+        session.getTransaction().commit();
+        return obj;
+    }
+
+    public List getlistStrap() {
+        List list;
+        session.beginTransaction();
+        list = session.createQuery("From Strap", Strap.class).list();
+        session.getTransaction().commit();
+        return list;
     }
 }
